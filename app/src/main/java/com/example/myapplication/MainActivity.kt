40db6d7d48ba6d33814678dc7a23f2727b06ae23
Jesús N <jesusn@Jesuss-MacBook-Air.local>
2024-04-15 12:56:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.util.UUID
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    Surface(color = MaterialTheme.colors.background) {
+        content()
+    }
+}
+
+@Composable
+fun MyScreenContent() {
+    var uniqueId by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { uniqueId = generarUUID() }) {
+            Text(text = "Generar Identificador Único")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Identificador Único:")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = uniqueId)
+    }
+}
+
+fun generarUUID(): String {
+    val uuid = UUID.randomUUID()
+    return uuid.toString()
+}
